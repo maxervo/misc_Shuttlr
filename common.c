20d24c38084e22eb9b282dede768c25b19b10418
JojoH2407 <joseph.kangou@gmail.com>
2016-11-28 12:28:23 +0100

    Add files via upload
    update 11/28
    creat creer_socket function

--- common.c
+++ common.c
@@ -1,14 +1,31 @@
 #include "common_impl.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int creer_socket(int prop, int *port_num) 
+
+int creer_socket(int prop, int *port_num)
 {
+  /* fonction de creation et d'attachement */
+  /* d'une nouvelle socket */
+  /* renvoie le numero de descripteur */
+  /* et modifie le parametre port_num */
    int fd = 0;
-   
-   /* fonction de creation et d'attachement */
-   /* d'une nouvelle socket */
-   /* renvoie le numero de descripteur */
-   /* et modifie le parametre port_num */
-   
+   struct sockaddr_in serv_addr;
+   memset(&serv_addr,0,sizeof(serv_addr));
+
+
+   serv_addr.sin_port=htons(0);
+   serv_addr.sin_family=AF_INET;
+   serv_addr.sin_addr.s_addr=INADDR_ANY;
+
+   fd=socket(AF_INET,SOCK_STREAM,0);
+   bind(fd, (struct sockaddr*) &serv_addr, sizeof(struct sockaddr_in));
+   *port_num=serv_addr.sin_port;
    return fd;
 }
 
