4f70801d99aa95cf07344b100278e8d3bda50a12
Maxime ALARDO <rstoke@khronos>
2016-12-05 14:26:58 +0100

    rebase dev

--- common.c
+++ common.c
@@ -8,12 +8,63 @@
 #include <netinet/in.h>
 
 
-int creer_socket(int prop, int *port_num)
+int create_socket(int prop, int *port_num)
 {
   /* fonction de creation et d'attachement */
   /* d'une nouvelle socket */
   /* renvoie le numero de descripteur */
   /* et modifie le parametre port_num */
+  int fd = 0;
+  if(prop==1) //TODO what magic number?
+  {
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_port=htons(*port_num);
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=INADDR_ANY;
+    fprintf(stdout,"Trying to connect with port_num %d\n",*port_num);
+    fd=socket(AF_INET,SOCK_STREAM,0);
+    if (fd == -1)
+    {
+      perror("socket");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return fd;
+ }
+
+//Rest of Joseph's code
+ /*while(connect(fd, (struct sockaddr *) & serv_addr, sizeof(serv_addr)) == -1)
+   printf("Socket %d connected in TCP/IP mode \n", fd);
+ {
+   perror("connect");
+ /*{ //Listenning socket
+   struct sockaddr_in addr_listen;
+   fd = socket(AF_INET, SOCK_STREAM, 0);
+   fprintf(stdout,"La socket %d est maintenant ouverte en mode TCP/IP\n", fd);
+   addr_listen.sin_family = AF_INET;
+   addr_listen.sin_port = htons(*port_num);
+   addr_listen.sin_addr.s_addr = INADDR_ANY;
+   while (bind(fd, (struct sockaddr *) &addr_listen, sizeof(addr_listen)) == -1)
+   {
+   perror("bind");
+   }
+   fprintf(stdout,"bind success\n");
+   if (listen(fd,listen_backlog) == -1)
+   {
+   perror("listen");
+   return -1;
+   }
+   fp*/
+
+//Backup original
+//{
+  /* fonction de creation et d'attachement */
+  /* d'une nouvelle socket */
+  /* renvoie le numero de descripteur */
+  /* et modifie le parametre port_num */
+  /*
    int fd = 0;
    struct sockaddr_in serv_addr;
    memset(&serv_addr,0,sizeof(serv_addr));
@@ -27,7 +78,10 @@ int creer_socket(int prop, int *port_num)
    bind(fd, (struct sockaddr*) &serv_addr, sizeof(struct sockaddr_in));
    *port_num=serv_addr.sin_port;
    return fd;
-}
+   */
+//}
+
+
 
 /* Vous pouvez ecrire ici toutes les fonctions */
 /* qui pourraient etre utilisees par le lanceur */
--- dsmwrap.c
+++ dsmwrap.c
@@ -1,15 +1,15 @@
 #include "common_impl.h"
 
 int main(int argc, char **argv)
-{   
+{
    /* processus intermediaire pour "nettoyer" */
    /* la liste des arguments qu'on va passer */
    /* a la commande a executer vraiment */
-   
+
    /* creation d'une socket pour se connecter au */
    /* au lanceur et envoyer/recevoir les infos */
-   /* necessaires pour la phase dsm_init */   
-   
+   /* necessaires pour la phase dsm_init */
+
    /* Envoi du nom de machine au lanceur */
 
    /* Envoi du pid au lanceur */
